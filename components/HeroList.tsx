263a1fb5a700fad372913fe7b95f72fd3f0f5e64
7akoda <takoda.godin@gmail.com>
2025-07-03 11:31:35 -0700

    feat: migrate from inline styles to react-native-unistyles with theme-based styling

--- components/HeroList.tsx
+++ components/HeroList.tsx
@@ -4,6 +4,7 @@ import { View, Image, Text, Button, FlatList } from "react-native"
 import * as Progress from 'react-native-progress';
 import { useState } from "react";
 import { StyleSheet } from "react-native-unistyles";
+import { appThemes } from "@/unistyles";
 
 export const HeroList = () => {
     const [sort, setSort] = useState('popular')
@@ -64,24 +65,25 @@ export const HeroList = () => {
         <Button onPress={()=> setSort('popular')} title={"sort by popularity"}></Button> :
         <Button  onPress={()=> setSort('winrate')} title={"sort by winrate"}></Button>}
         <FlatList data={sorted} renderItem={({item}) => 
-        <View style={{  flexDirection: "row", 
-          backgroundColor: "#343f3E", borderRadius: 4,}}>
+        <View
+        style={styles.heroListItem}
+      >
       <Image
         source={{ uri: item.images.minimap_image }}
         style={{ width: 20, height: 30,  alignSelf: "center" }}
       />
-      <Text style={{  color:"#EADEDA", paddingLeft: 7, fontSize: 13, alignSelf: "center" }}>{item.name}</Text>
+      <Text style={styles.heroText}>{item.name}</Text>
       <View style={{ flex: 1 }} /> 
 
       {sort == 'winrate' ? (
         <View style={{height: 10, alignSelf: "center"}}>
         <Progress.Bar progress={item.winRate/100} width={100} color="#94b0da" />
-        <Text style={{ alignSelf: "center", color:"#EADEDA", fontSize: 10 }}>{item.winRate}%</Text>
+        <Text style={styles.percentText}>{item.winRate}%</Text>
        </View>
       ) : (
         <View style={{height: 10, alignSelf: "center"}}>
             <Progress.Bar progress={(item.matches/ (totalHeroPicks / 12))} width={100} color="#94b0da"  />
-        <Text style={{ alignSelf: "center", color:"#EADEDA", fontSize: 10}}>{((item.matches / (totalHeroPicks /12)) * 100).toFixed(2)}%</Text>
+        <Text style={styles.percentText}>{((item.matches / (totalHeroPicks /12)) * 100).toFixed(2)}%</Text>
         </View>
       )}
 
@@ -95,5 +97,22 @@ export const HeroList = () => {
 }
 
 const styles = StyleSheet.create({
-    
+    percentText:{
+      alignSelf: 'center',
+      color: appThemes.light.colors.font,
+      fontSize:10
+    },
+    heroListItem:{
+      alignSelf: 'center',
+          flexDirection: "row",
+          backgroundColor: appThemes.light.colors.primary,
+          borderRadius: 4,
+          width: 300,
+          height: 40,           
+          paddingHorizontal: 8,
+          marginVertical: 1
+    },
+    heroText:{
+      color: appThemes.light.colors.font, paddingLeft: 7, fontSize: 13, alignSelf: "center" 
+    }
 })
