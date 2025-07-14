c387400986b28aab7db653387c38ea652e369356
7akoda <takoda.godin@gmail.com>
2025-07-14 11:33:10 -0700

    feat(api): add getPlayerHeroStats fetch function

---
+++ api/getPlayerHeroStats.ts
@@ -0,0 +1,5 @@
+export const getPlayerHeroStats = async (id: string) => {
+    const res = await fetch(`https://api.deadlock-api.com/v1/players/${id}/hero-stats`)
+    if (!res.ok) throw new Error('Failed to fetch');    
+    return res.json()
+}
