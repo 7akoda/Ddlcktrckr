436df2b209139f06aa258374057b8992a43ccecd
7akoda <takoda.godin@gmail.com>
2025-07-14 11:33:39 -0700

    feat(index): render HeroPlayerProfile with SteamID

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,16 @@ import {ScrollView} from "react-native";
 
 
 import { HeroList } from "@/components/HeroList";
+import { HeroPlayerProfile } from "@/components/HeroPlayerProfile";
 
 export default function Index() {
   
-
+const kodaNum = "76561198053289095"
  
   return (
-      <HeroList/>      
+    <>
+      <HeroPlayerProfile id={kodaNum}/>
+      </>
   );
 }
 
