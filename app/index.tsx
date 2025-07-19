1a71e7f5f11193ff8e83f2c52745ed01c9b42f52
7akoda <takoda.godin@gmail.com>
2025-07-18 21:59:00 -0700

    feat(index): add state to display selected hero profile dynamically

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,19 @@
 import {ScrollView} from "react-native";
-
-
 import { HeroList } from "@/components/HeroList";
-import { HeroPlayerProfile } from "@/components/HeroPlayerProfile";
+import { HeroPlayerProfile } from "@/components/HeroPlayerList";
+import { HeroProfile } from "@/components/HeroProfile";
+import { useState } from "react";
 
 export default function Index() {
   
 const kodaNum = "76561198053289095"
- 
+ const [hero, setHero] = useState(0)
   return (
-    <> 
-      <HeroPlayerProfile id={kodaNum}/>
+    <> {hero === 0? (<HeroPlayerProfile  onInspect = {(id) => setHero(id)}
+    id={kodaNum}/>) : <HeroProfile id={hero}/> }
+      
+     
      
       </>
   );
 }
-
-
-
-
