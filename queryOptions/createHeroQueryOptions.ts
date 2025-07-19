23edfda03bed97fd56fe4a4fabd3f41f31d9dd53
7akoda <takoda.godin@gmail.com>
2025-07-18 22:04:37 -0700

    feat(queryOptions) added createHeroDataByIdQueryOptions

--- queryOptions/createHeroQueryOptions.ts
+++ queryOptions/createHeroQueryOptions.ts
@@ -2,6 +2,7 @@ import { queryOptions } from "@tanstack/react-query"
 import { getHeroData } from "@/api/getHeroData"
 import { getHeroStats } from "@/api/getHeroStats"
 import { getPlayerHeroStats } from "@/api/getPlayerHeroStats"
+import { getHeroDataById } from "@/api/getHeroDataById"
 
 export const createHeroQueryOptions = () => {
     return queryOptions({
@@ -23,7 +24,14 @@ export const createPlayerHeroStatsQueryOptions = (id: string) => ({
       const [_key, playerId] = queryKey;
       return getPlayerHeroStats(playerId);
     }
-   
+  });
+  
+  export const createHeroDataByIdQueryOptions = (id: number) => ({
+    queryKey: ['heroDataById', id] as [string, number],
+    queryFn: async ({ queryKey }: { queryKey: [string, number] }) => {
+      const [_key, playerId] = queryKey;
+      return getHeroDataById(playerId);
+    }
   });
   
   
