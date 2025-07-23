2770db9db4cef8e33d18ff76a6cf4b180bd8c5b6
7akoda <takoda.godin@gmail.com>
2025-07-23 12:29:11 -0700

    refactor(index): remove HeroPlayerProfile and HeroProfile toggle, add HeroList inside SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,19 @@
-import {ScrollView} from "react-native";
+import { Button, SafeAreaView, ScrollView, View } from "react-native";
 import { HeroList } from "@/components/HeroList";
-import { HeroPlayerProfile } from "@/components/HeroPlayerList";
+import { HeroPlayerList } from "@/components/HeroPlayerList";
 import { HeroProfile } from "@/components/HeroProfile";
 import { useState } from "react";
+import { Link } from "expo-router";
 
 export default function Index() {
-  
-const kodaNum = "76561198053289095"
- const [hero, setHero] = useState(0)
-  return (
-    <> {hero === 0? (<HeroPlayerProfile  onInspect = {(id) => setHero(id)}
-    id={kodaNum}/>) : <HeroProfile id={hero}/> }
-      
-     
-     
-      </>
-  );
+	const kodaNum = "76561198053289095";
+	const [hero, setHero] = useState(0);
+	return (
+		<>
+			<SafeAreaView style={{ flex: 1 }}>
+				{/* <HeroPlayerList id={kodaNum} /> */}
+				<HeroList />
+			</SafeAreaView>
+		</>
+	);
 }
