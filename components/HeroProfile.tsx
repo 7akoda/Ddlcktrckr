93c42240bc9c0bef26782f7e54879cc699fabef2
7akoda <takoda.godin@gmail.com>
2025-07-30 21:55:10 -0700

    too lazy to do conventional commits today

--- components/HeroProfile.tsx
+++ components/HeroProfile.tsx
@@ -1,17 +1,39 @@
-import { createHeroDataByIdQueryOptions } from "@/queryOptions/createHeroQueryOptions";
+import {
+	createEnrichedHeroQueryOptions,
+	createHeroDataByIdQueryOptions,
+	createItemDataByIdQueryOptions,
+} from "@/queryOptions/createHeroQueryOptions";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "expo-router";
-import { View, Image, Text, FlatList, Button, Pressable } from "react-native";
+import {
+	View,
+	Image,
+	Text,
+	FlatList,
+	Button,
+	Pressable,
+	Dimensions,
+} from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 import { useUnistyles } from "react-native-unistyles";
 import { SvgUri } from "react-native-svg";
 import { SvgComponent } from "./DDLKSvg";
+import { Header } from "./Header";
+import { getHeroData } from "@/api/getHeroData";
 type Props = {
 	id: number;
 };
 
 export const HeroProfile = ({ id }: Props) => {
 	const { theme } = useUnistyles();
+	const screenHeight = Dimensions.get("window").height;
+
+	const {
+		data: heroStats,
+		error: heroStatsError,
+		isLoading: heroStatsLoading,
+		isError: heroStatsIsError,
+	} = useQuery(createEnrichedHeroQueryOptions());
 
 	const {
 		data: heroDataById,
@@ -20,53 +42,104 @@ export const HeroProfile = ({ id }: Props) => {
 		isError: heroDataByIdIsError,
 	} = useQuery(createHeroDataByIdQueryOptions(id));
 
-	if (heroDataByIdIsLoading) {
+	const {
+		data: itemDataById,
+		error: itemDataByIdError,
+		isLoading: itemDataByIdIsLoading,
+		isError: itemDataByIdIsError,
+	} = useQuery(createItemDataByIdQueryOptions(id));
+
+	if (heroDataByIdIsLoading || heroStatsLoading || itemDataByIdIsLoading) {
 		return <Text>Loading...</Text>;
 	}
 
-	if (heroDataByIdIsError) {
+	if (heroDataByIdIsError || heroStatsIsError || itemDataByIdIsError) {
 		console.error(heroDataByIdError);
+		console.error(heroStatsError);
+		console.error(itemDataByIdError);
+
 		return <Text>Failed to load player stats.</Text>;
 	}
 
+	const heroList = Array.isArray(heroStats) ? heroStats : [];
+
+	const heroIdMatch = heroList?.find((hero: any) => {
+		return hero.id === id;
+	});
+	console.log(heroIdMatch);
+	console.log(itemDataById);
+
 	return (
 		<View style={styles.backgroundView}>
-			<Link href="/" dismissTo asChild>
-				<Pressable style={{ justifyContent: "center", zIndex: 1 }}>
-					{({ pressed }) => (
-						<Text style={[styles.heroText, pressed && styles.heroTextFade]}>
-							back
-						</Text>
-					)}
-				</Pressable>
-			</Link>
+			<Header back={true} sortable={false} />
 			<Image
 				style={{
-					position: "absolute",
-					top: 33,
-					width: "140%",
+					width: "100%",
 					borderRadius: 4,
 					height: 70,
 					borderWidth: 2,
 					borderColor: theme.colors.primary,
+					zIndex: 2,
+					position: "absolute",
+					top: 48,
 				}}
 				source={require("../images/Background_Buildings.png")}></Image>
-			<Image
-				source={{ uri: heroDataById.images.icon_hero_card_webp }}
+			<View
+				style={{
+					flexDirection: "row",
+
+					width: "100%",
+				}}>
+				<Image
+					source={{ uri: heroDataById.images.icon_hero_card_webp }}
+					style={{
+						width: 35,
+						height: 35,
+						borderRadius: 4,
+						borderWidth: 2,
+						borderColor: theme.colors.primary,
+						marginTop: 17.5,
+						zIndex: 2,
+						marginLeft: 12,
+					}}
+				/>
+				<Text style={styles.heroTextBanner}>{heroDataById.name}</Text>
+			</View>
+			<View
 				style={{
 					position: "absolute",
-					top: 50,
-					left: 15,
-					width: 35,
-					height: 35,
-					alignSelf: "flex-start",
-					borderRadius: 4,
-					borderWidth: 2,
-					borderColor: theme.colors.primary,
-				}}
-			/>
-			<Text style={styles.heroTextBanner}>{heroDataById.name}</Text>
-			<SvgComponent></SvgComponent>
+					alignSelf: "center",
+					justifyContent: "center",
+					width: "100%",
+					height: screenHeight,
+					zIndex: 1,
+				}}>
+				{/* <SvgComponent></SvgComponent> */}
+			</View>
+			<View style={{ alignItems: "center", top: 20 }}>
+				<Text style={{ color: theme.colors.font }}>Abilities</Text>
+			</View>
+			<View style={styles.itemView}>
+				{itemDataById.map((itemData: any, index: number) => {
+					if (index > 0)
+						return (
+							<Image
+								style={{
+									flexDirection: "row",
+									width: 50,
+									height: 50,
+									zIndex: 2,
+									backgroundColor: theme.colors.background,
+									tintColor: theme.colors.accent,
+									borderRadius: 4,
+									borderWidth: 2,
+									borderColor: theme.colors.primary,
+								}}
+								source={{ uri: itemData.image_webp }}
+							/>
+						);
+				})}
+			</View>
 		</View>
 	);
 };
@@ -74,30 +147,35 @@ export const HeroProfile = ({ id }: Props) => {
 const styles = StyleSheet.create((theme) => ({
 	backgroundView: {
 		backgroundColor: theme.colors.background,
-		paddingBottom: 50,
 		flex: 1,
-		position: "relative",
 	},
 	heroText: {
 		color: theme.colors.font,
 		paddingLeft: 7,
 		fontSize: 13,
 		alignSelf: "center",
+		zIndex: 2,
 	},
 	heroTextBanner: {
-		position: "absolute",
 		color: theme.colors.bannerFont,
-		paddingLeft: 7,
 		fontSize: 13,
-		alignSelf: "center",
-		top: 58,
-		left: 50,
+		marginLeft: 8,
+		zIndex: 2,
+		marginTop: 26.25,
 	},
 	heroTextFade: {
 		color: theme.colors.font,
 		paddingLeft: 7,
 		fontSize: 13,
-		alignSelf: "center",
 		opacity: 0.6,
+		zIndex: 2,
+	},
+	itemView: {
+		top: 30,
+		width: "100%",
+		flexDirection: "row",
+		position: "relative",
+		zIndex: 2,
+		justifyContent: "space-evenly",
 	},
 }));
