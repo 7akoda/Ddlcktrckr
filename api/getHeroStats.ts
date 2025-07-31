4a5daef23bf3dc2d77552128f599730dbe1cd155
7akoda <takoda.godin@gmail.com>
2025-07-30 21:54:37 -0700

    too lazy to do conventional commits today

--- api/getHeroStats.ts
+++ api/getHeroStats.ts
@@ -1,4 +1,8 @@
 export const getHeroStats = async () => {
-const res = await fetch('https://api.deadlock-api.com/v1/analytics/hero-stats')
-return res.json()
-}
+	const res = await fetch(
+		"https://api.deadlock-api.com/v1/analytics/hero-stats"
+	);
+	if (!res.ok) throw new Error("Failed to fetch");
+
+	return res.json();
+};
