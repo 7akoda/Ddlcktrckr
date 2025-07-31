40c8d1a2d50dae2c79b0fca247a03eb29d9935a1
7akoda <takoda.godin@gmail.com>
2025-07-30 21:54:30 -0700

    too lazy to do conventional commits today

--- api/getHeroData.ts
+++ api/getHeroData.ts
@@ -1,8 +1,13 @@
 export const getHeroData = async () => {
-    const numbers = [1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 27, 31, 35, 50, 52, 58, 60]
-    const dataPromises = numbers.map(async (num) => {
-      const res = await fetch(`https://assets.deadlock-api.com/v2/heroes/${num}`); 
-      return await res.json()
-    })    
-    return await Promise.all(dataPromises)
-  }
+	const numbers = [
+		1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 27, 31,
+		35, 50, 52, 58, 60,
+	];
+	const dataPromises = numbers.map(async (num) => {
+		const res = await fetch(`https://assets.deadlock-api.com/v2/heroes/${num}`);
+		if (!res.ok) throw new Error("Failed to fetch");
+
+		return await res.json();
+	});
+	return await Promise.all(dataPromises);
+};
