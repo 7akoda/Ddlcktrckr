4043bc245a940f7cca0b157be244bc733a3a9bf4
7akoda <takoda.godin@gmail.com>
2025-07-30 21:54:58 -0700

    too lazy to do conventional commits today

--- components/HeroList.tsx
+++ components/HeroList.tsx
@@ -1,8 +1,9 @@
 import {
+	createEnrichedHeroQueryOptions,
 	createHeroQueryOptions,
 	createHeroStatsQueryOptions,
 } from "@/queryOptions/createHeroQueryOptions";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { View, Image, Text, FlatList, Pressable } from "react-native";
 import * as Progress from "react-native-progress";
 import { useState } from "react";
@@ -14,45 +15,30 @@ import { Header } from "./Header";
 export const HeroList = () => {
 	const [sort, setSort] = useState(true);
 	const { theme } = useUnistyles();
-
 	const {
 		data: heroStats,
 		error: heroStatsError,
 		isLoading: heroStatsLoading,
 		isError: heroStatsIsError,
-	} = useQuery(createHeroStatsQueryOptions());
+	} = useQuery(createEnrichedHeroQueryOptions());
 
-	const {
-		data: heroData,
-		error: heroDataError,
-		isLoading: heroDataLoading,
-		isError: heroDataIsError,
-	} = useQuery(createHeroQueryOptions());
-
-	if (heroStatsLoading || heroDataLoading) {
+	if (heroStatsLoading) {
 		return <Text style={{ color: "#EADEDA" }}>Loading heroes...</Text>;
 	}
 
-	if (heroStatsIsError || heroDataIsError) {
+	if (heroStatsIsError) {
 		console.error("Hero stats error:", heroStatsError);
-		console.error("Hero data error:", heroDataError);
+
 		return <Text style={{ color: "#EADEDA" }}>Failed to load hero data.</Text>;
 	}
 
 	let sorted: any[] = [];
 
-	const heroes = heroData!.map((hero) => {
-		const stats = heroStats.find(
-			(stat: { hero_id: number }) => stat.hero_id === hero.id
-		);
-		const winRate = stats
-			? ((stats.wins / stats.matches) * 100).toFixed(2)
-			: null;
-		const matches = stats?.matches ?? 0;
-		return { ...hero, winRate, matches };
-	});
-	const sortedWinrate = heroes.slice().sort((a, b) => b.winRate - a.winRate);
-	const sortedPopular = heroes.slice().sort((a, b) => b.matches - a.matches);
+	const heroList = Array.isArray(heroStats) ? heroStats : [];
+	const totalHeroPicks = heroList.reduce((sum, hero) => sum + hero.matches, 0);
+
+	const sortedWinrate = heroList.slice().sort((a, b) => b.winRate - a.winRate);
+	const sortedPopular = heroList.slice().sort((a, b) => b.matches - a.matches);
 
 	if (sort === true) {
 		sorted = sortedWinrate;
@@ -60,12 +46,14 @@ export const HeroList = () => {
 		sorted = sortedPopular;
 	}
 
-	const totalHeroPicks = sorted.reduce((sum, hero) => sum + hero.matches, 0);
-
 	return (
 		<View style={styles.primaryView}>
-			<Header back={false} sortList={() => setSort(!sort)} />
-
+			<Header
+				back={false}
+				sortable={true}
+				sortList={() => setSort(!sort)}
+				sortText={sort ? "Winrate" : "Pickrate"}
+			/>
 			<FlatList
 				data={sorted}
 				renderItem={({ item }) => (
@@ -84,7 +72,7 @@ export const HeroList = () => {
 						<Link
 							href={{
 								pathname: `/[id]`,
-								params: { id: item.hero_id },
+								params: { id: item.id },
 							}}
 							push
 							asChild>
@@ -115,9 +103,7 @@ export const HeroList = () => {
 									width={100}
 									color={theme.colors.accent}
 								/>
-								<Text style={styles.percentText}>
-									{((item.matches / (totalHeroPicks / 12)) * 100).toFixed(2)}%
-								</Text>
+								<Text style={styles.percentText}>{item.popularity}%</Text>
 							</View>
 						)}
 					</View>
