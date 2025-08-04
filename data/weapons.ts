0210c5e2abc91b41e0344aac9a555f3b8a0aa81f
7akoda <takoda.godin@gmail.com>
2025-08-03 23:43:10 -0700

    feat(heroes): add weapon names and image requires to hero data

---
+++ data/weapons.ts
@@ -0,0 +1,166 @@
+type Hero = {
+	id: number;
+	name: string;
+	weaponImage: any;
+	weaponName: string;
+};
+
+export const heroWeapons: { [key: string]: Hero } = {
+	"1": {
+		id: 1,
+		name: "Infernus",
+		weaponName: "Incendiary Remarks",
+		weaponImage: require("../images/weapons/320px-Infernus_Weapon.png"),
+	},
+	"2": {
+		id: 2,
+		name: "Seven",
+		weaponName: "Cold Calculus",
+		weaponImage: require("../images/weapons/320px-Seven_Weapon.png"),
+	},
+	"3": {
+		id: 3,
+		name: "Vindicta",
+		weaponName: "Wallflower's Revenge",
+		weaponImage: require("../images/weapons/320px-Vindicta_Weapon.png"),
+	},
+	"4": {
+		id: 4,
+		name: "Lady Geist",
+		weaponName: "Johann's Gift",
+		weaponImage: require("../images/weapons/320px-Lady_Geist_Weapon.png"),
+	},
+	"6": {
+		id: 6,
+		name: "Abrams",
+		weaponName: "Case Closed",
+		weaponImage: require("../images/weapons/320px-Abrams_Weapon.png"),
+	},
+	"7": {
+		id: 7,
+		name: "Wraith",
+		weaponName: "The Rake",
+		weaponImage: require("../images/weapons/320px-Wraith_Weapon.png"),
+	},
+	"8": {
+		id: 8,
+		name: "McGinnis",
+		weaponName: "Services Rendered",
+		weaponImage: require("../images/weapons/320px-McGinnis_Weapon.png"),
+	},
+	"10": {
+		id: 10,
+		name: "Paradox",
+		weaponName: "Trade Secret",
+		weaponImage: require("../images/weapons/320px-Paradox_Weapon.png"),
+	},
+	"11": {
+		id: 11,
+		name: "Dynamo",
+		weaponName: "The Vonnegun",
+		weaponImage: require("../images/weapons/320px-Dynamo_Weapon.png"),
+	},
+	"12": {
+		id: 12,
+		name: "Kelvin",
+		weaponName: "Expedition's End",
+		weaponImage: require("../images/weapons/320px-Kelvin_Weapon.png"),
+	},
+	"13": {
+		id: 13,
+		name: "Haze",
+		weaponName: "Censure & Penance",
+		weaponImage: require("../images/weapons/320px-Haze_Weapon.png"),
+	},
+	"14": {
+		id: 14,
+		name: "Holliday",
+		weaponName: "Doc's Orders",
+		weaponImage: require("../images/weapons/320px-Holliday_Weapon.png"),
+	},
+	"15": {
+		id: 15,
+		name: "Bebop",
+		weaponName: "Humble Pie",
+		weaponImage: require("../images/weapons/320px-Bebop_Weapon.png"),
+	},
+	"16": {
+		id: 16,
+		name: "Calico",
+		weaponName: "Sweetest Lie",
+		weaponImage: require("../images/weapons/320px-Calico_Weapon.png"),
+	},
+	"17": {
+		id: 17,
+		name: "Grey Talon",
+		weaponName: "The Mourning Comes",
+		weaponImage: require("../images/weapons/320px-Grey_Talon_Weapon.png"),
+	},
+	"18": {
+		id: 18,
+		name: "Mo & Krill",
+		weaponName: "Yellow Canary",
+		weaponImage: require("../images/weapons/320px-MoKrill_Weapon.png"),
+	},
+	"19": {
+		id: 19,
+		name: "Shiv",
+		weaponName: "Busted Flush",
+		weaponImage: require("../images/weapons/320px-Shiv_Weapon.png"),
+	},
+	"20": {
+		id: 20,
+		name: "Ivy",
+		weaponName: "Protector's Hand",
+
+		weaponImage: require("../images/weapons/320px-Ivy_Weapon.png"),
+	},
+	"25": {
+		id: 25,
+		name: "Warden",
+		weaponName: "Freedom's Cost",
+		weaponImage: require("../images/weapons/320px-Warden_Weapon.png"),
+	},
+	"27": {
+		id: 27,
+		name: "Yamato",
+		weaponName: "Sister's Lament",
+		weaponImage: require("../images/weapons/320px-Yamato_Weapon.png"),
+	},
+	"31": {
+		id: 31,
+		name: "Lash",
+		weaponName: "Tale of The Tape",
+		weaponImage: require("../images/weapons/320px-Lash_Weapon.png"),
+	},
+	"35": {
+		id: 35,
+		name: "Viscous",
+		weaponName: "Deep Sea Interpreter",
+		weaponImage: require("../images/weapons/320px-Viscous_Weapon.png"),
+	},
+	"50": {
+		id: 50,
+		name: "Pocket",
+		weaponName: "The Black Sheep",
+		weaponImage: require("../images/weapons/320px-Pocket_Weapon.png"),
+	},
+	"52": {
+		id: 52,
+		name: "Mirage",
+		weaponName: "Promises Kept",
+		weaponImage: require("../images/weapons/320px-Placeholder_Weapon.png"),
+	},
+	"58": {
+		id: 58,
+		name: "Vyper",
+		weaponName: "The Rattler",
+		weaponImage: require("../images/weapons/320px-Vyper_Weapon.png"),
+	},
+	"60": {
+		id: 60,
+		name: "Sinclair",
+		weaponName: "Magic Wand",
+		weaponImage: require("../images/weapons/320px-Sinclair_Weapon.png"),
+	},
+};
