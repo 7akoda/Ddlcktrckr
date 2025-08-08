d450574525e40e33efdca322ba7e7ac2897b0489
7akoda <takoda.godin@gmail.com>
2025-08-08 12:08:01 -0700

    I will do proper commits when i am paid to do proper commits

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
 import { Stack } from "expo-router";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-
-  return (
-  <QueryClientProvider client={queryClient}>
-    <Stack screenOptions={{ headerShown:false}} />
-    </QueryClientProvider>);
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Stack screenOptions={{ headerShown: false }} />
+		</QueryClientProvider>
+	);
 }
