3be6dc2270d797b0f0592a16ca15a7f095f10fdd
7akoda <takoda.godin@gmail.com>
2025-08-08 12:08:17 -0700

    I will do proper commits when i am paid to do proper commits

---
+++ components/CustomText.tsx
@@ -0,0 +1,9 @@
+import { Text, TextProps } from "react-native";
+import { useUnistyles } from "react-native-unistyles";
+
+export const CustomText = (props: TextProps) => {
+	const { theme } = useUnistyles();
+	return (
+		<Text {...props} style={[{ fontFamily: theme.fontFamily }, props.style]} />
+	);
+};
