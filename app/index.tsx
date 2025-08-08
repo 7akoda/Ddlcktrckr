24355b033632cc4977434fb9c81bad94b03dd52f
7akoda <takoda.godin@gmail.com>
2025-08-08 12:08:05 -0700

    I will do proper commits when i am paid to do proper commits

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,18 @@
-import { Button, SafeAreaView, ScrollView, View } from "react-native";
+import { Button, SafeAreaView, ScrollView, View, Text } from "react-native";
 import { HeroList } from "@/components/HeroList";
 import { HeroPlayerList } from "@/components/HeroPlayerList";
 import { HeroProfile } from "@/components/HeroProfile";
 import { useState } from "react";
 import { Link } from "expo-router";
+import { useFonts } from "@expo-google-fonts/rye";
+import { Rye_400Regular } from "@expo-google-fonts/rye";
 
 export default function Index() {
 	const kodaNum = "76561198053289095";
 	const [hero, setHero] = useState(0);
+	let [fontsLoaded] = useFonts({
+		Rye_400Regular,
+	});
 	return (
 		<>
 			<SafeAreaView style={{ flex: 1 }}>
