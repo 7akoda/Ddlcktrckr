26a5a2b3fc655c9fd91da3db7e677e6184f8e46d
7akoda <takoda.godin@gmail.com>
2025-08-15 12:39:32 -0700

    to

--- hooks/useHeroData.ts
+++ hooks/useHeroData.ts
@@ -1,27 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
 import {
 	createEnrichedHeroQueryOptions,
-	createHeroDataByIdQueryOptions,
-	createItemDataByIdQueryOptions,
+	createHeroStatsQueryOptions,
 } from "@/queryOptions/createHeroQueryOptions";
+import { useQuery } from "@tanstack/react-query";
 
-export const useHeroData = (id: number) => {
-	const heroStats = useQuery(createEnrichedHeroQueryOptions());
-	const heroDataById = useQuery(createHeroDataByIdQueryOptions(id));
-	const itemDataById = useQuery(createItemDataByIdQueryOptions(id));
+export const useHeroData = () => {
+	const heroData = useQuery(createEnrichedHeroQueryOptions());
+	const heroStats = useQuery(createHeroStatsQueryOptions());
 
-	const isLoading =
-		heroStats.isLoading || heroDataById.isLoading || itemDataById.isLoading;
-	const isError =
-		heroStats.isError || heroDataById.isError || itemDataById.isError;
-	const error = heroStats.error || heroDataById.error || itemDataById.error;
+	const isLoading = heroData.isLoading || heroStats.isLoading;
+
+	const isError = heroData.isError || heroStats.isError;
+
+	const error = heroData.error || heroStats.error;
 
 	return {
+		heroData: heroData.data,
 		heroStats: heroStats.data,
-		heroDataById: heroDataById.data,
-		itemDataById: itemDataById.data,
-		isLoading,
-		isError,
 		error,
+		isError,
+		isLoading,
 	};
 };
