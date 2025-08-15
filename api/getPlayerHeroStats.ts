11adca4e3c0c3a3e57bff159c6a51182a0b11af4
7akoda <takoda.godin@gmail.com>
2025-08-15 12:38:48 -0700

    i

--- api/getPlayerHeroStats.ts
+++ api/getPlayerHeroStats.ts
@@ -1,5 +1,7 @@
 export const getPlayerHeroStats = async (id: string) => {
-    const res = await fetch(`https://api.deadlock-api.com/v1/players/${id}/hero-stats`)
-    if (!res.ok) throw new Error('Failed to fetch');    
-    return res.json()
-}
+	const res = await fetch(
+		`https://api.deadlock-api.com/v1/players/${id}/hero-stats`
+	);
+	if (!res.ok) throw new Error("Failed to fetch");
+	return res.json();
+};
