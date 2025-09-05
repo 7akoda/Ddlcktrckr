3e1af7c413148b9f8c362b6465fe0936dde5cb8d
7akoda <takoda.godin@gmail.com>
2025-09-05 13:23:38 -0700

    🦅

--- api/getHeroData.ts
+++ api/getHeroData.ts
@@ -1,7 +1,7 @@
 export const getHeroData = async () => {
 	const numbers = [
 		1, 2, 3, 4, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 27, 31,
-		35, 50, 52, 58, 60, 63, 72,
+		35, 50, 52, 58, 60, 63, 64, 66, 67, 69, 72,
 	];
 	const dataPromises = numbers.map(async (num) => {
 		const res = await fetch(`https://assets.deadlock-api.com/v2/heroes/${num}`);
