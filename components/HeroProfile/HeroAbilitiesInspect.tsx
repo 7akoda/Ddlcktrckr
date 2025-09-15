d2a552d72db592ada41c8dcdd9881b30b799d696
7akoda <takoda.godin@gmail.com>
2025-09-15 12:40:15 -0700

    dont care

--- components/HeroProfile/HeroAbilitiesInspect.tsx
+++ components/HeroProfile/HeroAbilitiesInspect.tsx
@@ -4,12 +4,14 @@ import { CustomText } from "../CustomText";
 import { Dimensions, View } from "react-native";
 import { useUnistyles } from "react-native-unistyles";
 import { heroMoves } from "../../data/moves";
+import { Image } from "react-native";
 type Props = {
 	id: number;
 	match: any;
+	abilityInspect: number;
 };
 
-export const HeroAbilitiesInspect = ({ id, match }: Props) => {
+export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 	const { theme } = useUnistyles();
 	const { itemDataById, isIdError, isIdLoading, idError } = useHeroDataById(id);
 	if (isIdLoading) return <LoadingIcon />;
@@ -22,6 +24,16 @@ export const HeroAbilitiesInspect = ({ id, match }: Props) => {
 			.replace(/&lt;/g, "<")
 			.replace(/&gt;/g, ">")
 			.replace(/&quot;/g, '"')
+			.replace(/{g:citadel_binding:'Attack'}/g, "Attack ")
+			.replace(/{g:citadel_binding:'AltCast'}/g, "Alt Cast ")
+			.replace(/{g:citadel_binding:'MoveForward'}/g, "Move forward ")
+			.replace(/{g:citadel_binding:'MoveDown'}/g, "Move down ")
+			.replace(/{g:citadel_binding:'Mantle'}/g, " mantle ")
+			.replace(/orAttack/, "or Attack")
+			.replace(/{g:citadel_binding:'Ability1'}/g, " Ability 1 ")
+			.replace(/{g:citadel_binding:'Ability2'}/g, "Ability 2 ")
+			.replace(/{g:citadel_binding:'Ability3'}/g, "Ability 3 ")
+			.replace(/{g:citadel_binding:'Ability4'}/g, "Ability 4 ")
 			.replace(/\s+/g, " ")
 			.trim();
 	};
@@ -29,13 +41,13 @@ export const HeroAbilitiesInspect = ({ id, match }: Props) => {
 	const matchedHero = heroMoves.find((hero) => {
 		return hero.id == id;
 	});
+	const numbers = [1, 2, 5];
 
 	return (
 		<View
 			style={{
 				backgroundColor: theme.colors.background,
 				width: 350,
-				height: 300,
 				zIndex: 4,
 				alignSelf: "center",
 				borderRadius: 4,
@@ -47,34 +59,151 @@ export const HeroAbilitiesInspect = ({ id, match }: Props) => {
 					zIndex: 6,
 					textAlign: "center",
 					color: theme.colors.font,
-					fontSize: 8,
+					fontSize: 10,
 					padding: 8,
 					margin: 12,
 					borderWidth: 2,
 					borderRadius: 4,
 					borderColor: theme.colors.accent,
 				}}>
-				{stripHtml(match.description?.desc) || "No description available"}
+				{match.description.desc
+					? stripHtml(match.description.desc)
+					: "No description available"}
 			</CustomText>
-			{matchedHero?.signature1.upgrades.map((upgrades) => {
-				return (
-					<CustomText
-						key={id}
-						style={{
-							zIndex: 6,
-							textAlign: "center",
-							color: theme.colors.font,
-							fontSize: 8,
-							padding: 8,
-							margin: 12,
-							borderWidth: 2,
-							borderRadius: 4,
-							borderColor: theme.colors.accent,
-						}}>
-						{upgrades}
-					</CustomText>
-				);
-			})}
+			<View
+				style={{
+					flexDirection: "row",
+					marginBottom: 12,
+				}}>
+				{numbers.map((num) => {
+					return (
+						<View
+							style={{
+								width: 116.6,
+								flexDirection: "row",
+								justifyContent: "center",
+							}}>
+							<Image
+								key={num}
+								source={require("../../images/20px-Ability_point.png")}
+								style={{
+									alignSelf: "center",
+									width: 15,
+									height: 15,
+									tintColor: theme.colors.accent,
+								}}
+							/>
+							<CustomText
+								style={{
+									color: theme.colors.accent,
+									fontSize: 14,
+									paddingLeft: 4,
+									alignSelf: "center",
+								}}>
+								{num}
+							</CustomText>
+						</View>
+					);
+				})}
+			</View>
+			<View
+				style={{
+					flexDirection: "row",
+					alignSelf: "center",
+				}}>
+				{abilityInspect === 0 &&
+					matchedHero?.signature1.upgrades.map((upgrade, index) => (
+						<>
+							<View
+								style={{
+									flexDirection: "row",
+									alignSelf: "center",
+								}}>
+								<CustomText
+									key={upgrade}
+									style={{
+										zIndex: 6,
+										textAlign: "center",
+										color: theme.colors.font,
+										fontSize: 10,
+										width: 116.6,
+										height: 70,
+										padding: 4,
+										borderWidth: 1,
+										borderRadius: 4,
+										borderColor: theme.colors.primary,
+									}}>
+									{upgrade}
+								</CustomText>
+							</View>
+						</>
+					))}
+
+				{abilityInspect === 1 &&
+					matchedHero?.signature2.upgrades.map((upgrade, index) => (
+						<>
+							<CustomText
+								key={upgrade}
+								style={{
+									zIndex: 6,
+									textAlign: "center",
+									color: theme.colors.font,
+									fontSize: 10,
+									width: 116.6,
+									height: 70,
+									padding: 4,
+									borderWidth: 1,
+									borderRadius: 4,
+									borderColor: theme.colors.primary,
+								}}>
+								{upgrade}
+							</CustomText>
+						</>
+					))}
+				{abilityInspect === 2 &&
+					matchedHero?.signature3.upgrades.map((upgrade, index) => (
+						<>
+							<CustomText
+								key={upgrade}
+								style={{
+									zIndex: 6,
+									textAlign: "center",
+									color: theme.colors.font,
+									fontSize: 10,
+									width: 116.6,
+									height: 70,
+									padding: 4,
+									borderWidth: 1,
+									borderRadius: 4,
+									borderColor: theme.colors.primary,
+								}}>
+								{upgrade}
+							</CustomText>
+						</>
+					))}
+
+				{abilityInspect === 3 &&
+					matchedHero?.signature4.upgrades.map((upgrade, index) => (
+						<>
+							<CustomText
+								key={upgrade}
+								style={{
+									zIndex: 6,
+									textAlign: "center",
+									color: theme.colors.font,
+									fontSize: 10,
+									width: 116.6,
+									height: 70,
+									padding: 4,
+									borderWidth: 1,
+									borderRadius: 4,
+									borderColor: theme.colors.primary,
+								}}>
+								{upgrade}
+							</CustomText>
+						</>
+					))}
+			</View>
 		</View>
 	);
 };
