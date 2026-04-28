9a656cfbc794b8831f0d8ded72610020c36a625c
7akoda <takoda.godin@gmail.com>
2026-04-27 21:28:23 -0700

    refactor(HeroAbilitiesInspect): extracted into multiple components and get functions, cleaned up some typescript logic

--- components/HeroProfile/HeroAbilitiesInspect.tsx
+++ components/HeroProfile/HeroAbilitiesInspect.tsx
@@ -1,9 +1,8 @@
 import { useHeroDataById } from "@/hooks/useHeroDataById";
 import { LoadingIcon } from "../LoadingIcon";
 import { CustomText } from "../CustomText";
-import { Dimensions, Pressable, View } from "react-native";
+import { View } from "react-native";
 import { useUnistyles } from "react-native-unistyles";
-import { heroMoves } from "../../data/moves";
 import { Image } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 import { AbilityDetails } from "./AbilityDetails";
@@ -11,22 +10,22 @@ import { useState } from "react";
 import {
 	cleanDecimals,
 	cleanDescription,
-	cleanUpgrade,
 	valueNumberizer,
 } from "@/api/decimaldescriptionTransform";
+import { getProperties } from "@/api/getHeroAbilityInspectInfo";
+import { AbilityPress } from "./AbilityPress";
 type Props = {
 	id: number;
 	match: any;
-	abilityInspect: number;
 };
 
-export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
+export const HeroAbilitiesInspect = ({ id, match }: Props) => {
 	const { theme } = useUnistyles();
 	const [upgrade, setUpgrade] = useState([]);
 	const [upgradeSelected, setUpgradeSelected] = useState(-1);
 
 	const { heroDataById, isIdError, isIdLoading, idError } = useHeroDataById(
-		String(id)
+		String(id),
 	);
 	if (isIdLoading) return <LoadingIcon />;
 	if (isIdError) return <CustomText>{String(idError)}</CustomText>;
@@ -34,11 +33,11 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 	const rawProperties = Object.entries(match?.properties).map(
 		([key, value]) => {
 			return [key, value];
-		}
+		},
 	);
 	const numbers = [1, 2, 5];
 
-	const foundUpgrade = (detail: any) => {
+	const foundUpgrade = (detail: any): any => {
 		return upgrade?.find((u: any) => u.name == detail);
 	};
 
@@ -67,38 +66,6 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 		return [key, value];
 	});
 
-	const impPropsInf0_1 =
-		match.tooltip_details?.info_sections?.[0]?.properties_block?.[0]
-			?.properties;
-	const impPropInf0_2 =
-		match.tooltip_details?.info_sections?.[0]?.properties_block?.[1]
-			?.properties;
-	const impPropsInf0_3 =
-		match.tooltip_details?.info_sections?.[0]?.properties_block?.[2]
-			?.properties;
-	const impPropsInf1_0 =
-		match.tooltip_details?.info_sections?.[1]?.properties_block?.[0]
-			?.properties;
-	const impPropsInf1_1 =
-		match.tooltip_details?.info_sections?.[1]?.properties_block?.[1]
-			?.properties;
-	const impPropsInf2_0 =
-		match.tooltip_details?.info_sections?.[2]?.properties_block?.[0]
-			?.properties;
-	const impPropsInf2_1 =
-		match.tooltip_details?.info_sections?.[2]?.properties_block?.[1]
-			?.properties;
-
-	let impPropsArray = [
-		impPropsInf0_1,
-		impPropInf0_2,
-		impPropsInf0_3,
-		impPropsInf1_0,
-		impPropsInf1_1,
-		impPropsInf2_0,
-		impPropsInf2_1,
-	].flat();
-
 	const basicPropsArr1 =
 		match.tooltip_details?.info_sections?.[0]?.basic_properties;
 	const basicPropsArr2 =
@@ -106,39 +73,24 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 	const basicPropsArr3 =
 		match.tooltip_details?.info_sections?.[2]?.basic_properties;
 
-	let importantProperties = [];
-
-	if (basicPropsArr3 && !basicPropsArr1 && !basicPropsArr2) {
-		const basicProps = basicPropsArr3.map((property: any) => {
-			return { important_property: property };
-		});
-		importantProperties = basicProps ? basicProps : [];
-	}
-	if (basicPropsArr1) {
-		const basicProps = basicPropsArr1.map((property: any) => {
-			return { important_property: property };
-		});
-		importantProperties = basicProps ? basicProps : [];
-	}
-	if (basicPropsArr2) {
-		const basicProps = basicPropsArr2.map((property: any) => {
-			return { important_property: property };
-		});
-		importantProperties = basicProps ? basicProps : [];
-	}
+	let importantProperties: any[] = [];
+	const toImportant = (arr: any[]) =>
+		arr.map((property: any) => ({ important_property: property }));
 
 	if (basicPropsArr1 && basicPropsArr2) {
-		const basicProps = basicPropsArr2
-			.concat(basicPropsArr1)
-			.map((property: any) => {
-				return { important_property: property };
-			});
-		importantProperties = basicProps ? basicProps : [];
+		importantProperties = toImportant(basicPropsArr2.concat(basicPropsArr1));
+	} else if (basicPropsArr1) {
+		importantProperties = toImportant(basicPropsArr1);
+	} else if (basicPropsArr2) {
+		importantProperties = toImportant(basicPropsArr2);
+	} else if (basicPropsArr3) {
+		importantProperties = toImportant(basicPropsArr3);
 	}
-
+	let impPropsArray = getProperties(match);
 	impPropsArray = impPropsArray.filter((property: any) => {
 		return property !== undefined;
 	});
+
 	importantProperties = importantProperties.concat(impPropsArray);
 
 	importantProperties = importantProperties.map((importantProperty: any) => {
@@ -152,7 +104,7 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 	});
 
 	propertyArray = propertyArray.filter((p: any) =>
-		importantProperties.some((m: any) => p[0] == m && p[0] !== "SigilRadius")
+		importantProperties.some((m: any) => p[0] == m && p[0] !== "SigilRadius"),
 	);
 
 	propertyArray.map((p: any) => {
@@ -165,17 +117,9 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 
 	propertyArray = propertyArray.filter(
 		(p: any) =>
-			valueNumberizer(p[1]?.value) !== 0 && valueNumberizer(p[1]?.value) !== -1
+			valueNumberizer(p[1]?.value) !== 0 && valueNumberizer(p[1]?.value) !== -1,
 	);
 
-	if (upgrade !== undefined && upgrade) {
-		const shit = propertyArray.map((propertyOMG: any) => {
-			return upgrade.find((up: any) => {
-				return up.name == propertyOMG[0];
-			});
-		});
-	}
-	console.log(upgrade);
 	const upgradeRelationArray = upgrades.map((up: any) => {
 		return rawProperties.find((u: any) => up[0].name == u[0]);
 	});
@@ -264,7 +208,7 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 									{foundUpgrade(ability[0]) !== undefined &&
 									ability[1] !== undefined
 										? cleanDecimals(valueNumberizer(ability[1].value)) +
-										  valueNumberizer(foundUpgrade(ability[0]).bonus)
+											valueNumberizer(foundUpgrade(ability[0]).bonus)
 										: cleanDecimals(valueNumberizer(ability[1].value))}
 									{ability[1]?.postfix == " m" ? "m" : ability[1]?.postfix}
 								</CustomText>
@@ -321,86 +265,17 @@ export const HeroAbilitiesInspect = ({ id, match, abilityInspect }: Props) => {
 					width: 345,
 					marginBottom: 4,
 				}}>
-				{upgrades.map((uMap: any, index: number) => {
-					return (
-						<View
-							key={index}
-							style={
-								index <= upgradeSelected
-									? styles.inspectAbilitySelected
-									: styles.inspectAbility
-							}>
-							<Pressable
-								style={{ width: 110, height: 70, alignContent: "center" }}
-								onPress={() =>
-									index == upgradeSelected && upgradeSelected == 2
-										? (setUpgrade(
-												upgradesIndexOne.map((i: any) => {
-													return { name: i.name, bonus: i.bonus };
-												})
-										  ),
-										  setUpgradeSelected(1))
-										: index == upgradeSelected && upgradeSelected == 1
-										? (setUpgrade(
-												upgrades[0].map((i: any) => {
-													return { name: i.name, bonus: i.bonus };
-												})
-										  ),
-										  setUpgradeSelected(0))
-										: index == upgradeSelected && upgradeSelected == 0
-										? (setUpgrade([]), setUpgradeSelected(-1))
-										: index == 0
-										? (setUpgrade(
-												upgrades[0].map((i: any) => {
-													return { name: i.name, bonus: i.bonus };
-												})
-										  ),
-										  setUpgradeSelected(0))
-										: index == 1
-										? (setUpgrade(
-												upgradesIndexOne.map((i: any) => {
-													return { name: i.name, bonus: i.bonus };
-												})
-										  ),
-										  setUpgradeSelected(1))
-										: (setUpgrade(
-												upgradesIndexTwo.map((i: any) => {
-													return { name: i.name, bonus: i.bonus };
-												})
-										  ),
-										  setUpgradeSelected(2))
-								}>
-								{match.description.t1_desc && index + 1 == 1 ? (
-									<CustomText style={styles.inspectAbilityText}>
-										{cleanUpgrade(match.description.t1_desc)}
-									</CustomText>
-								) : match.description.t2_desc && index + 1 == 2 ? (
-									<CustomText style={styles.inspectAbilityText}>
-										{cleanUpgrade(match.description.t2_desc)}
-									</CustomText>
-								) : match.description.t3_desc && index + 1 == 3 ? (
-									<CustomText style={styles.inspectAbilityText}>
-										{cleanUpgrade(match.description.t3_desc)}
-									</CustomText>
-								) : (
-									uMap.map((u: any, index1: number) => {
-										return (
-											<CustomText
-												style={styles.inspectAbilityText}
-												key={index1}>
-												{valueNumberizer(u.bonus)}
-												{upgradeRelationArray[index][1].postfix == " m"
-													? "m"
-													: upgradeRelationArray[index][1].postfix}{" "}
-												{upgradeRelationArray[index][1].label}
-											</CustomText>
-										);
-									})
-								)}
-							</Pressable>
-						</View>
-					);
-				})}
+				<AbilityPress
+					match={match}
+					setUpgrade={setUpgrade}
+					setUpgradeSelected={setUpgradeSelected}
+					styles={styles}
+					upgradeRelationArray={upgradeRelationArray}
+					upgradeSelected={upgradeSelected}
+					upgrades={upgrades}
+					upgradesIndexOne={upgradesIndexOne}
+					upgradesIndexTwo={upgradesIndexTwo}
+				/>
 			</View>
 		</View>
 	);
