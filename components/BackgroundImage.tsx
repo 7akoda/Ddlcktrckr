4d431d0e0cafc3d53c0179b70390410bee883a51
7akoda <takoda.godin@gmail.com>
2026-04-28 21:40:33 -0700

    feat(backgroundImage): extracted from index

---
+++ components/BackgroundImage.tsx
@@ -0,0 +1,24 @@
+import { View, Image } from "react-native";
+import { useUnistyles } from "react-native-unistyles";
+export const BackgroundImage = () => {
+	const { rt } = useUnistyles();
+	return (
+		<View style={{ position: "absolute" }}>
+			{rt.themeName === "dark" ? (
+				<>
+					<Image
+						style={{ width: 1390, height: 900, opacity: 0.2 }}
+						source={require("../images/Background_Buildings.png")}></Image>
+				</>
+			) : rt.themeName === "light" ? (
+				<Image
+					style={{
+						width: 1390,
+						height: 900,
+						opacity: 0.4,
+					}}
+					source={require("../images/Background_Buildings_Light.png")}></Image>
+			) : null}
+		</View>
+	);
+};
