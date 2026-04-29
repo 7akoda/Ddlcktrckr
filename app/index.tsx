fb2464e09571264905a8704ac36abf9f82c1929c
7akoda <takoda.godin@gmail.com>
2026-04-28 21:39:41 -0700

    refactor(index): extracted index into multiple components for clarity

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,21 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { HeroList } from "@/components/HeroList";
-import { HeroPlayerList } from "@/components/HeroPlayerList";
-import { useEffect, useState } from "react";
-import { EBGaramond_400Regular } from "@expo-google-fonts/eb-garamond/400Regular";
-import { EBGaramond_600SemiBold } from "@expo-google-fonts/eb-garamond/600SemiBold";
-import { EBGaramond_800ExtraBold } from "@expo-google-fonts/eb-garamond/800ExtraBold";
-import { useFonts } from "@expo-google-fonts/eb-garamond/useFonts";
+import { useState } from "react";
 import { Shader1 } from "@/components/Shader";
-import { useUnistyles } from "react-native-unistyles";
-import { ItemList } from "@/components/ItemList";
-import { View, TextInput, Image, Button } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import * as Linking from "expo-linking";
 import React from "react";
 import { BottomNavBar } from "@/components/BottomNavBar";
+import { BackgroundImage } from "@/components/BackgroundImage";
+import { MainContent } from "@/components/MainContent";
 
 export default function Index() {
-	const { theme, rt } = useUnistyles();
-	const [data, setData] = useState<string | null>(null);
+	const [data, setData] = useState<string>("");
+	const [settings, setSettings] = useState(false);
+	const [selected, setSelected] = useState("World");
 
-	let [fontsLoaded] = useFonts({
-		EBGaramond_400Regular,
-		EBGaramond_600SemiBold,
-		EBGaramond_800ExtraBold,
-	});
+	if (data == null && selected == "User") {
+		() => setSettings(true);
+	}
 
 	const RES = async (): Promise<string | null> => {
 		try {
@@ -53,37 +45,28 @@ export default function Index() {
 			console.log(id);
 		}
 	};
-	const [selected, setSelected] = useState("World");
+	const handleNoAuth = () => {
+		setSettings((prev) => !prev);
+		setSelected("World");
+	};
 
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<Shader1 />
-			<View style={{ position: "absolute" }}>
-				{rt.themeName === "dark" ? (
-					<>
-						<Image
-							style={{ width: 1390, height: 900, opacity: 0.2 }}
-							source={require("../images/Background_Buildings.png")}></Image>
-					</>
-				) : rt.themeName === "light" ? (
-					<Image
-						style={{
-							width: 1390,
-							height: 900,
-							opacity: 0.4,
-						}}
-						source={require("../images/Background_Buildings_Light.png")}></Image>
-				) : null}
-			</View>
-			{selected === "User" ? (
-				<HeroPlayerList id={data} steamAuth={handleLogin} />
-			) : selected === "Items" ? (
-				<ItemList steamAuth={handleLogin} />
-			) : (
-				<HeroList steamAuth={handleLogin} />
-			)}
-
-			<BottomNavBar selected={selected} setSelected={setSelected} />
+			<BackgroundImage />
+			<MainContent
+				data={data}
+				handleLogin={handleLogin}
+				handleNoAuth={handleNoAuth}
+				selected={selected}
+				settings={settings}
+			/>
+			<BottomNavBar
+				data={data}
+				setSettings={setSettings}
+				selected={selected}
+				setSelected={setSelected}
+			/>
 		</SafeAreaView>
 	);
 }
