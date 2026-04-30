6b81de99862df12b40ea79fdd63ef201b1755bd1
7akoda <takoda.godin@gmail.com>
2026-04-29 21:39:46 -0700

    feat(layout) reverted back from (tabs)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,15 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect } from "react";
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Stack
-				screenOptions={{ headerShown: false, animation: "fade_from_bottom" }}
-			/>
+				screenOptions={{
+					headerShown: false,
+					animation: "fade_from_bottom",
+				}}></Stack>
 		</QueryClientProvider>
 	);
 }
