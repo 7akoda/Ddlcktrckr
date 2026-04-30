6afd5bf2c33e6b6adf39f94882bf8c3b78d8f674
7akoda <takoda.godin@gmail.com>
2026-04-29 21:41:36 -0700

    feat(BottomNavBar) implemented memo and useCallback

--- components/BottomNavBar.tsx
+++ components/BottomNavBar.tsx
@@ -1,9 +1,8 @@
 import { useUnistyles } from "react-native-unistyles";
-import { CustomText } from "./CustomText";
-import { Pressable, View } from "react-native";
+import { Pressable } from "react-native";
 import { BlurView } from "expo-blur";
 import { Book, BookOpen, Globe, SquareUserRound } from "lucide-react-native";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, memo, SetStateAction, useCallback, useState } from "react";
 
 type BottomNavBarType = {
 	setSelected: Dispatch<SetStateAction<string>>;
@@ -11,93 +10,97 @@ type BottomNavBarType = {
 	setSettings: Dispatch<SetStateAction<boolean>>;
 	data: string;
 };
+export const BottomNavBar = memo(
+	({ setSelected, selected, setSettings, data }: BottomNavBarType) => {
+		const { theme } = useUnistyles();
 
-export const BottomNavBar = ({
-	setSelected,
-	selected,
-	setSettings,
-	data,
-}: BottomNavBarType) => {
-	const { theme, rt } = useUnistyles();
+		const handleNoAuth = useCallback(() => {
+			setSelected("User");
+			setSettings(true);
+		}, []);
 
-	const handleNoAuth = () => {
-		setSelected("User");
-		setSettings(true);
-	};
-	return (
-		<BlurView
-			intensity={25}
-			style={{
-				bottom: 0,
-				height: "9%",
-				width: "100%",
-				position: "absolute",
-				borderTopWidth: 1,
-				flexDirection: "row",
-			}}>
-			<Pressable
-				onPress={() => (data.length < 1 ? handleNoAuth() : setSelected("User"))}
+		return (
+			<BlurView
+				intensity={25}
 				style={{
-					width: "24.7%",
-					marginHorizontal: "4.3%",
-					justifyContent: "center",
-					borderTopColor: selected == "User" ? theme.colors.accent : "#000000",
-					borderTopWidth: selected == "User" ? 1 : 0,
+					bottom: 0,
+					height: "9%",
+					width: "100%",
+					position: "absolute",
+					borderTopWidth: 1,
+					flexDirection: "row",
 				}}>
-				<SquareUserRound
-					style={{ alignSelf: "center" }}
-					strokeWidth={1}
-					color={selected == "User" ? theme.colors.accent : theme.colors.font}
-					size={34}
-				/>
-			</Pressable>
-			<Pressable
-				onPress={() => setSelected("World")}
-				style={{
-					width: "24.7%",
-					marginHorizontal: "4.3%",
+				<Pressable
+					onPress={() =>
+						data.length < 1 ? handleNoAuth() : setSelected("User")
+					}
+					style={{
+						width: "24.7%",
+						marginHorizontal: "4.3%",
+						justifyContent: "center",
+						borderTopColor:
+							selected == "User" ? theme.colors.accent : "#000000",
+						borderTopWidth: selected == "User" ? 1 : 0,
+					}}>
+					<SquareUserRound
+						style={{ alignSelf: "center" }}
+						strokeWidth={1}
+						color={selected == "User" ? theme.colors.accent : theme.colors.font}
+						size={34}
+					/>
+				</Pressable>
+				<Pressable
+					onPress={() => setSelected("World")}
+					style={{
+						width: "24.7%",
+						marginHorizontal: "4.3%",
 
-					justifyContent: "center",
-					borderTopColor: selected == "World" ? theme.colors.accent : "#000000",
-					borderTopWidth: selected == "World" ? 1 : 0,
-				}}>
-				<Globe
-					style={{ alignSelf: "center" }}
-					color={selected == "World" ? theme.colors.accent : theme.colors.font}
-					strokeWidth={1}
-					size={32}
-				/>
-			</Pressable>
-			<Pressable
-				onPress={() => setSelected("Items")}
-				style={{
-					width: "24.7%",
-					marginHorizontal: "4.3%",
-
-					justifyContent: "center",
-					borderTopColor: selected == "Items" ? theme.colors.accent : "#000000",
-					borderTopWidth: selected == "Items" ? 1 : 0,
-				}}>
-				{selected === "Items" ? (
-					<BookOpen
+						justifyContent: "center",
+						borderTopColor:
+							selected == "World" ? theme.colors.accent : "#000000",
+						borderTopWidth: selected == "World" ? 1 : 0,
+					}}>
+					<Globe
 						style={{ alignSelf: "center" }}
 						color={
-							selected == "Items" ? theme.colors.accent : theme.colors.font
+							selected == "World" ? theme.colors.accent : theme.colors.font
 						}
 						strokeWidth={1}
-						size={33}
+						size={32}
 					/>
-				) : (
-					<Book
-						style={{ alignSelf: "center" }}
-						color={
-							selected == "Items" ? theme.colors.accent : theme.colors.font
-						}
-						strokeWidth={1}
-						size={30}
-					/>
-				)}
-			</Pressable>
-		</BlurView>
-	);
-};
+				</Pressable>
+				<Pressable
+					onPress={() => setSelected("Items")}
+					style={{
+						width: "24.7%",
+						marginHorizontal: "4.3%",
+
+						justifyContent: "center",
+						borderTopColor:
+							selected == "Items" ? theme.colors.accent : "#000000",
+						borderTopWidth: selected == "Items" ? 1 : 0,
+					}}>
+					{selected === "Items" ? (
+						<BookOpen
+							style={{ alignSelf: "center" }}
+							color={
+								selected == "Items" ? theme.colors.accent : theme.colors.font
+							}
+							strokeWidth={1}
+							size={33}
+						/>
+					) : (
+						<Book
+							style={{ alignSelf: "center" }}
+							color={
+								selected == "Items" ? theme.colors.accent : theme.colors.font
+							}
+							strokeWidth={1}
+							size={30}
+						/>
+					)}
+				</Pressable>
+			</BlurView>
+		);
+	},
+);
