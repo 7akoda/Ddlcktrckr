b2a4b8efe1127767b27182dabadc77da58c93a76
7akoda <takoda.godin@gmail.com>
2026-05-01 20:34:49 -0700

    Chore: removed dead code and moved components around

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -5,8 +5,9 @@ import { Popup } from "./Popup";
 import { ItemList } from "./ItemList";
 import { useUnistyles } from "react-native-unistyles";
 import { LoadingIcon } from "./LoadingIcon";
-import { LoadingIconSmall } from "./LoadingIconSmall";
+
 import { HeroPlayerList } from "./HeroPlayerList";
+import { SteamSvg } from "./svgComponents/SteamSVG";
 
 type MainType = {
 	selected: string;
@@ -26,43 +27,82 @@ export const MainContent = ({
 	const { theme } = useUnistyles();
 	return (
 		<>
-			{selected === "User" && data.length < 1 && settings ? (
-				<>
-					<HeroList handleLogin={handleLogin} />
+			{data.length < 1 && selected == "User" ? (
+				<View style={{ height: "100%" }}>
 					<Popup settings={settings} handlePress={handleNoAuth}>
 						<View>
-							<Pressable
-								onPress={handleLogin}
+							<CustomText
 								style={{
-									backgroundColor: theme.colors.font,
-									width: 165,
+									fontSize: 10,
+									fontFamily: theme.fontFamily.extraBold,
+									color: theme.colors.font,
+									top: 7,
+									marginHorizontal: 5,
+									alignSelf: "center",
+									marginBottom: 5,
+								}}>
+								User data unavailable
+							</CustomText>
+							<CustomText
+								style={{
+									fontSize: 10,
+									fontFamily: theme.fontFamily.extraBold,
+									color: theme.colors.font,
+									top: 7,
+									marginHorizontal: 5,
+									alignSelf: "center",
+									marginBottom: 15,
+								}}>
+								Authentication required
+							</CustomText>
+						</View>
+						<Pressable
+							onPress={handleLogin}
+							style={{
+								width: 150,
+								height: 40,
+								flexDirection: "row",
+								borderRadius: 24,
+								borderColor: theme.colors.font,
+								borderWidth: 1,
+								alignSelf: "center",
+							}}>
+							<CustomText
+								style={{
+									fontSize: 19,
+									fontFamily: theme.fontFamily.extraBold,
+									color: theme.colors.font,
+									top: 7,
+									marginHorizontal: 5,
+								}}>
+								Sign in
+							</CustomText>
+							<View style={{ flex: 1 }} />
+							<View
+								style={{
+									backgroundColor: theme.colors.background,
+									width: 55,
+									overflow: "hidden",
 									height: 30,
 									alignSelf: "center",
-									borderRadius: 12,
+									borderRadius: 24,
 									borderWidth: 1,
 									borderColor: theme.colors.font,
 									justifyContent: "center",
+									marginRight: 5,
 								}}>
-								<CustomText
-									style={{
-										color: theme.colors.background,
-										alignSelf: "center",
-										textAlign: "center",
-										fontSize: 18,
-									}}>
-									Sign in with Steam
-								</CustomText>
-							</Pressable>
-						</View>
-						<LoadingIconSmall />
+								<SteamSvg fill={theme.colors.font} />
+							</View>
+						</Pressable>
 					</Popup>
-				</>
+					<LoadingIcon />
+				</View>
 			) : selected === "Items" ? (
-				<ItemList handleLogin={handleLogin} />
+				<ItemList />
 			) : selected === "User" && data.length > 1 ? (
-				<HeroPlayerList id={data} handleLogin={handleLogin} />
+				<HeroPlayerList id={data} />
 			) : (
-				<HeroList handleLogin={handleLogin} />
+				<HeroList />
 			)}
 		</>
 	);
