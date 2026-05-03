69473d2610d17a6bd9fb08df5970642f7b362c70
7akoda <takoda.godin@gmail.com>
2026-05-03 09:01:39 -0700

    chore: eslint changes

--- api/getFix.tsx
+++ api/getFix.tsx
@@ -1,21 +1,21 @@
 export const getPostfix = (postfix: string, value: string, prop: any) => {
-	return (postfix == "m" &&
+	return (postfix === "m" &&
 		prop.css_class === "move_speed" &&
-		value[value.length - 1] == "m") ||
-		(postfix[0] == " " && prop.css_class === "move_speed")
+		value[value.length - 1] === "m") ||
+		(postfix[0] === " " && prop.css_class === "move_speed")
 		? "/s"
-		: postfix == value[value.length - 1] ||
-			  postfix[postfix.length - 1] == value[value.length - 1]
+		: postfix === value[value.length - 1] ||
+			  postfix[postfix.length - 1] === value[value.length - 1]
 			? ""
-			: postfix[0] == " "
+			: postfix[0] === " "
 				? postfix.slice(1)
 				: postfix;
 };
 
 export const getPrefix = (prefix: string, value: string) => {
-	return prefix == value[0]
+	return prefix === value[0]
 		? ""
-		: value[0] == "-"
+		: value[0] === "-"
 			? ""
 			: prefix === "{s:sign}"
 				? "+"
