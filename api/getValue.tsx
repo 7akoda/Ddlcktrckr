013e110c2e03cfafe1732cdc747b498a1d88d020
7akoda <takoda.godin@gmail.com>
2026-05-03 09:02:19 -0700

    chore: eslint changes

--- api/getValue.tsx
+++ api/getValue.tsx
@@ -1,5 +1,5 @@
 export const getValue = (value: string, postfix: string) => {
-	return value[value.length - 1] == postfix[0] &&
+	return value[value.length - 1] === postfix[0] &&
 		postfix !== value[value.length - 1]
 		? value.slice(0, value.length - 1)
 		: value;
