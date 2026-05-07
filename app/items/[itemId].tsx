8f1e6ee86ff7bc54fc4226bf4f3b485c7793e402
7akoda <takoda.godin@gmail.com>
2026-05-07 10:59:32 -0700

    style: updated background image styling

--- app/items/[itemId].tsx
+++ app/items/[itemId].tsx
@@ -1,35 +1,21 @@
+import { BackgroundImage } from "@/components/BackgroundImage";
 import { ItemProfile } from "@/components/ItemProfile";
 import { Shader1 } from "@/components/Shader";
 import { useLocalSearchParams } from "expo-router";
-import { View, Image } from "react-native";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useUnistyles } from "react-native-unistyles";
 
 export default function ItemId() {
 	const { itemId } = useLocalSearchParams();
-	const { rt, theme } = useUnistyles();
+	const { theme } = useUnistyles();
 	return (
 		<>
 			<SafeAreaView
 				style={{ flex: 1, backgroundColor: theme.colors.background }}>
 				<Shader1 />
-				<View style={{ position: "absolute" }}>
-					{rt.themeName === "dark" ? (
-						<>
-							<Image
-								style={{ width: 1390, height: 900, opacity: 0.2 }}
-								source={require("../../images/Background_Buildings.png")}></Image>
-						</>
-					) : rt.themeName === "light" ? (
-						<Image
-							style={{
-								width: 1390,
-								height: 900,
-								opacity: 0.3,
-							}}
-							source={require("../../images/Background_Buildings.png")}></Image>
-					) : null}
-				</View>
+				<BackgroundImage />
+
 				{itemId && <ItemProfile itemId={itemId} />}
 			</SafeAreaView>
 		</>
