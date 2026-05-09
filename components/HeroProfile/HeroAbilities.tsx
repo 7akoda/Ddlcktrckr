93df60cd7640002902d605207c0d898938f81304
7akoda <takoda.godin@gmail.com>
2026-05-09 16:57:28 -0700

    style: fixed unintended wrapping of ability names

--- components/HeroProfile/HeroAbilities.tsx
+++ components/HeroProfile/HeroAbilities.tsx
@@ -1,25 +1,41 @@
-import { Pressable, View, Image } from "react-native";
+import { Pressable, Image, View } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 import { CustomText } from "../CustomText";
+import Animated, {
+	createAnimatedComponent,
+	useAnimatedStyle,
+	useSharedValue,
+} from "react-native-reanimated";
+import { BouncePress } from "@/animations/Bounce";
+import { useState } from "react";
 
 type Props = {
-	id: number;
 	handleShade: () => void;
 	matchedItem: any;
 	setSelectedAbilityIndex: (matchedItem: any) => void;
 };
-
+const AnimatedPressable = createAnimatedComponent(Pressable);
 export const HeroAbilities = ({
-	id,
 	handleShade,
 	matchedItem,
 	setSelectedAbilityIndex,
 }: Props) => {
+	const [isColumn, setIsColumn] = useState(false);
+
+	const size = useSharedValue(1);
+	const animatedStyles = useAnimatedStyle(() => {
+		return {
+			transform: [{ scale: size.value }],
+		};
+	});
+	const abilitySplit = matchedItem.name.split(" ");
+
 	return (
-		<View>
-			<Pressable
+		<Animated.View style={animatedStyles}>
+			<AnimatedPressable
 				onPress={() => {
 					{
+						BouncePress(size, 75);
 						handleShade();
 						setSelectedAbilityIndex(matchedItem);
 					}
@@ -28,9 +44,39 @@ export const HeroAbilities = ({
 					style={styles.abilityImage}
 					source={{ uri: matchedItem.image_webp }}
 				/>
-			</Pressable>
-			<CustomText style={styles.abilityText}>{matchedItem.name}</CustomText>
-		</View>
+			</AnimatedPressable>
+			<View
+				onLayout={(e) => {
+					const width = e.nativeEvent.layout.width;
+					if (width > 80) {
+						setIsColumn(true);
+					}
+				}}
+				style={{
+					flexDirection: isColumn ? "column" : "row",
+					alignSelf: "center",
+				}}>
+				{abilitySplit.length === 3 ? (
+					<>
+						<CustomText style={styles.abilityText}>
+							{abilitySplit[0]} {abilitySplit[1]}
+						</CustomText>
+						<CustomText style={styles.abilityText}>
+							{abilitySplit[2]}
+						</CustomText>
+					</>
+				) : (
+					abilitySplit.map((ability: string) => {
+						return (
+							<CustomText key={ability} style={styles.abilityText}>
+								{ability}
+								{isColumn ? "" : " "}
+							</CustomText>
+						);
+					})
+				)}
+			</View>
+		</Animated.View>
 	);
 };
 
@@ -50,10 +96,9 @@ const styles = StyleSheet.create((theme) => ({
 	abilityText: {
 		color: "#FFFFFF",
 		fontFamily: theme.fontFamily.semiBold,
-		fontSize: 8,
 		alignSelf: "center",
-		width: 60,
 		textAlign: "center",
 		zIndex: 2,
+		fontSize: 12,
 	},
 }));
